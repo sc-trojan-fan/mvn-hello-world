cc83b1b3b30b81dceecef374bd33b160ee85c9c4
sc-trojan-fan <dwcurtis1984@gmail.com>
2016-08-14 02:13:57 -0500

    Update App.java

--- src/main/java/com/devops/app/App.java
+++ src/main/java/com/devops/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello DevOps World!" );
+        System.out.println( "This is a new line from Darryl!" );
     }
 }
